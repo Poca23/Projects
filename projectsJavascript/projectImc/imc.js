e6860d817880dcb93e8d1a675dd85a55ea572579
Naudin Claire <cndweb37@gmail.com>
2024-10-25 19:19:55 +0200

    mvProjectImc

--- projectsJavascript/projectImc/imc.js
+++ projectsJavascript/projectImc/imc.js
@@ -9,14 +9,20 @@ function calculDeLimc() {
   resultats.innerHTML = "L'IMC calculé est égal à " + imc + " Kg/m²";
 
   if (imc > 30) {
-    interpretation.innerHTML = "D'après l'OMS,  l'individu souffre d'obésité.";
+    interpretation.innerHTML = "D'après l'OMS,  tu es dans le brin mon pote 😱";
   } else if (imc > 25) {
     interpretation.innerHTML =
-      "D'après l'OMS,  l'individu souffre de surpoids.";
-  } else if (imc > 20) {
-    interpretation.innerHTML = "D'après l'OMS,  RAS, GG, continue comme ça.";
+      "D'après l'OMS,  vive la salle de sport et les salades à gogo 😅";
+  } else if (imc >= 20) {
+    interpretation.innerHTML = "D'après l'OMS,  RAS, GG, continue comme ça 🥳";
+  } else if (imc > 18) {
+    interpretation.innerHTML =
+      "D'après l'OMS,  ça va, mais pas moins wesh, tu veux pas être une brindille non plus 😉";
+  } else if (imc > 17) {
+    interpretation.innerHTML =
+      "D'après l'OMS,  une bonne cuisse de poulet te ferait pas du mal 🙂";
   } else {
     interpretation.innerHTML =
-      "D'après l'OMS,  ça va, mais pas moins wesh, tu veux pas être une brindille non plus.";
+      "Heu ... c'est ton ombre ou c'est toi que je vois ? 🤐";
   }
 }

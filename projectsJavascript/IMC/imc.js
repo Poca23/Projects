20cc9a813861da69caa01c4a32aa540b57635ce2
Naudin Claire <cndweb37@gmail.com>
2024-10-25 15:38:22 +0200

    mvFilesProjectsImc

--- projectsJavascript/IMC/imc.js
+++ projectsJavascript/IMC/imc.js
@@ -1,3 +1,22 @@
 // //////////////////////////////////////
 // IMC
 // //////////////////////////////////////
+function calculDeLimc() {
+  let masse = inputMasse.value;
+  let taille = inputTaille.value;
+  let imc = Math.round(masse / (taille * taille));
+
+  resultats.innerHTML = "L'IMC calculé est égal à " + imc + " Kg/m²";
+
+  if (imc > 30) {
+    interpretation.innerHTML = "D'après l'OMS,  l'individu souffre d'obésité.";
+  } else if (imc > 25) {
+    interpretation.innerHTML =
+      "D'après l'OMS,  l'individu souffre de surpoids.";
+  } else if (imc > 20) {
+    interpretation.innerHTML = "D'après l'OMS,  RAS, GG, continue comme ça.";
+  } else {
+    interpretation.innerHTML =
+      "D'après l'OMS,  ça va, mais pas moins wesh, tu veux pas être une brindille non plus.";
+  }
+}

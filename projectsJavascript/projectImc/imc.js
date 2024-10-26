bc69c6ca753910a9e7eb8d7b36526cd454962158
Naudin Claire <cndweb37@gmail.com>
2024-10-26 17:53:27 +0200

    addFolderProjectAzerType

--- projectsJavascript/projectImc/imc.js
+++ projectsJavascript/projectImc/imc.js
@@ -8,14 +8,20 @@ function calculDeLimc() {
 
   resultats.innerHTML = "L'IMC calculé est égal à " + imc + " Kg/m²";
 
-  if (imc > 30) {
-    interpretation.innerHTML = "D'après l'OMS,  tu es dans le brin mon pote 😱";
-  } else if (imc > 25) {
+  if (imc > 40) {
+    interpretation.innerHTML =
+      "D'après l'OMS,  tu veux quoi sur ta pierre tombale ?";
+  } else if (imc > 35) {
     interpretation.innerHTML =
       "D'après l'OMS,  vive la salle de sport et les salades à gogo 😅";
-  } else if (imc >= 20) {
+  } else if (imc > 30) {
+    interpretation.innerHTML = "D'après l'OMS,  t'as un peu le pas lourd non ?";
+  } else if (imc > 25) {
+    interpretation.innerHTML =
+      "D'après l'OMS,  tes petites poignées d'amour en font craquer sur ton passage";
+  } else if (imc > 20) {
     interpretation.innerHTML = "D'après l'OMS,  RAS, GG, continue comme ça 🥳";
-  } else if (imc > 18) {
+  } else if (imc > 18.5) {
     interpretation.innerHTML =
       "D'après l'OMS,  ça va, mais pas moins wesh, tu veux pas être une brindille non plus 😉";
   } else if (imc > 17) {

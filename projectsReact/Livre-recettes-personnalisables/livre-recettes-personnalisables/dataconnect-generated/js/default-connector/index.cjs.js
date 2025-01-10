a7d037d15b9597cbe349f29adf88cd5f88105b2c
Claire Naudin <cndweb37@gmail.com>
2025-01-10 15:43:45 +0100

    Suppression des fichiers de sauvegarde et des fichiers de test obsolètes

--- projectsReact/Livre-recettes-personnalisables/livre-recettes-personnalisables/dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'livre-recettes-personnalisables',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
